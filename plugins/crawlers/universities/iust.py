cddbfb3a077ddd72eaf976619789d56c41846877
hoseyn <87471826+hoseyn-chegeni@users.noreply.github.com>
2024-07-22 15:31:20 +0330

    add schools urls

--- plugins/crawlers/universities/iust.py
+++ plugins/crawlers/universities/iust.py
@@ -10,6 +10,24 @@ from schemas import colleges
 class ElmSanatCrawler(University):
     def __init__(self) -> None:
         self.url = "https://www.iust.ac.ir/"
+        self.electrical_engineering = "https://ee.iust.ac.ir/"
+        self.mechanical_engineering = "https://mech.iust.ac.ir/"
+        self.automotive_engineering = "https://aed.iust.ac.ir/"
+        self.railway_engineering = "https://railway.iust.ac.ir/"
+        self.mathematics_and_computer_science = "https://math.iust.ac.ir/"
+        self.chemistry = "https://chemistry.iust.ac.ir/"
+        self.chemical_petroleum_and_gas_engineering = "http://chem_eng.iust.ac.ir/"
+        self.industrial_engineering = "https://ie.iust.ac.ir/"
+        self.civil_engineering = "https://civil.iust.ac.ir/"
+        self.advanced_technologies = "https://fn.iust.ac.ir/"
+        self.physics = "https://physics.iust.ac.ir/"
+        self.computer_engineering = "https://ce.iust.ac.ir/"
+        self.architecture_and_environmental_design = "https://www.iust.ac.ir/index.php?sid=27&slc_lang=fa"
+        self.ثconomy = "https://pe.iust.ac.ir/"
+        self.metallurgy_and_materials = "http://meteng.iust.ac.ir/"
+    
+
+
 
     def get_colleges(self) -> Generator[colleges.CollegeData, None, None]:
         response = check_connection(requests.get, self.url + "persons.php?slc_lang=fa&sid=1&list_sections=1/fa/")
