94e37abafb041b2354d40a354bfc51b770f0b63f
hoseyn <87471826+hoseyn-chegeni@users.noreply.github.com>
2024-07-31 09:53:51 +0330

    edit college data schema

--- plugins/schemas/colleges.py
+++ plugins/schemas/colleges.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
-
+from typing import Optional
 
 class CollegeData(BaseModel):
-    href: str
-    value: str
+    href: Optional[str] = None
+    value: Optional[str] = None
