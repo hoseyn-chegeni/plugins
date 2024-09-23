0fa739ddc22f545abcd280addabc52135441a5c0
hoseyn <87471826+hoseyn-chegeni@users.noreply.github.com>
2024-09-23 15:51:29 +0330

    ajabshir prof done

--- plugins/crawlers/universities/azad/ajab_shir.py
+++ plugins/crawlers/universities/azad/ajab_shir.py
@@ -20,7 +20,29 @@ class Crawler(University):
         pass
 
     def get_professors(self):
-        pass
+        with sync_playwright() as p:
+            browser = p.chromium.launch(headless=True)
+            page = browser.new_page()
+            page.goto(
+                "https://ajabshir.iau.ir/fa/grid/3/%D8%A7%D8%B9%D8%B6%D8%A7%DB%8C-%D9%87%DB%8C%D8%A7%D8%AA-%D8%B9%D9%84%D9%85%DB%8C"
+            )
+            page.wait_for_selector("table.table-striped")
+            page_content = page.content()
+            browser.close()
+
+        soup = BeautifulSoup(page_content, "html.parser")
+        table = soup.find("table", class_="table-striped")
+        rows = table.find("tbody").find_all("tr")
+        for row in rows:
+
+            columns = row.find_all("td")
+
+            name = columns[1].get_text(strip=True)
+            rank = columns[2].get_text(strip=True)
+            major = columns[3].get_text(strip=True)
+
+            professor = Professor(full_name=name, rank=rank, major=major)
+            yield professor
 
     def get_professor_page(self, link) -> Professor:
         pass
